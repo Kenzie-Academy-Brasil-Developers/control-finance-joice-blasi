ed8aca0f24c31e7ddb1f0244d00adfb0d6693e1b
Joice Blasi <joiceblasi@hotmail.com>
2022-10-03 07:14:58 -0300

    finalizado

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,166 @@
-/* Desenvolva sua lógica aqui */
+function addValue() {
+  const buttonNewValueHeader = document.querySelector(".button-nav");
+  buttonNewValueHeader.addEventListener("click", openModal);
+}
+
+function addFinancialRecord(list) {
+  const section = document.querySelector(".new-section");
+  section.innerText = "";
+  const ul = document.createElement("ul");
+  ul.classList.add("container");
+  section.appendChild(ul);
+
+  list.forEach((element) => {
+    const li = document.createElement("li");
+    const pValue = document.createElement("p");
+    const div = document.createElement("div");
+    const pType = document.createElement("p");
+    const buttonTrash = document.createElement("button");
+
+    li.classList.add(
+      "item-registered-value",
+      "margin-5",
+      "flex",
+      "align-center",
+      "justify-between",
+      "padding-5"
+    );
+    li.id = element.id;
+    pValue.classList.add("color-grey-1", "text-2-medium");
+    div.classList.add("width-50", "flex", "justify-between", "align-center");
+    pType.classList.add(
+      "type-item",
+      "bg-grey-4",
+      "color-grey-2",
+      "text-2-regular",
+      "padding-5"
+    );
+    buttonTrash.classList.add("button-trash");
+
+    pValue.innerText = `R$ ${element.value}`;
+    if (element.categoryID === 1) {
+      pType.innerText = "Entrada";
+    } else if (element.categoryID === 2) {
+      pType.innerText = "Saída";
+    }
+    buttonTrash.addEventListener("click", () => {
+      buttonTrash.id = element.id;
+      if (buttonTrash.id === li.id) {
+        const index = insertedValues.indexOf(element, 0);
+        insertedValues.splice(index, 1);
+        li.remove();
+        addValueSum(insertedValues);
+        addFinancialRecord(insertedValues);
+        noRecordedValues(insertedValues);
+      }
+    });
+
+    ul.appendChild(li);
+    li.append(pValue, div);
+    div.append(pType, buttonTrash);
+    addValueSum(insertedValues);
+  });
+}
+
+function addValueSum(list) {
+  const pValueSum = document.querySelector(".value-sum");
+  let acc = 0;
+  list.forEach((element) => {
+    if (element.categoryID === 1) {
+      acc += element.value;
+    } else if (element.categoryID === 2) {
+      acc -= element.value;
+    }
+  });
+  pValueSum.innerText = `R$ ${acc}`;
+  if (acc < 0) {
+    pValueSum.classList.add("color-red");
+  } else {
+    pValueSum.classList.remove("color-red");
+  }
+}
+
+function noRecordedValues(list) {
+  if (list.length === 0) {
+    const section = document.querySelector(".new-section");
+
+    const div = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const button = document.createElement("button");
+
+    div.classList.add(
+      "no-registered-values",
+      "container",
+      "flex",
+      "flex-col",
+      "justify-center",
+      "align-center",
+      "margin-1",
+      "padding-1"
+    );
+    h2.classList.add("text-no-registered", "color-grey-1", "title-2-bold");
+    button.classList.add(
+      "button-no-registered",
+      "margin-1",
+      "color-grey-1",
+      "text-2-medium"
+    );
+
+    h2.innerText = "Nenhum valor cadastrado";
+    button.innerText = "Registrar novo valor";
+    button.addEventListener("click", openModal);
+
+    section.appendChild(div);
+    div.append(h2, button);
+  }
+}
+
+const filterByCategory = function (list, category) {
+  const filterList = list.filter((element) => {
+    return element.categoryID === category;
+  });
+  return filterList;
+};
+
+const all = document.querySelector(".all");
+all.addEventListener("click", () => {
+  all.classList.toggle("button-filter");
+  const deposit = document.querySelector(".deposit");
+  const withdraw = document.querySelector(".withdraw");
+  deposit.classList.remove("button-filter");
+  withdraw.classList.remove("button-filter");
+  addFinancialRecord(insertedValues);
+  addValueSum(insertedValues);
+  noRecordedValues(insertedValues);
+});
+
+const deposit = document.querySelector(".deposit");
+deposit.addEventListener("click", () => {
+  deposit.classList.toggle("button-filter");
+  const all = document.querySelector(".all");
+  const withdraw = document.querySelector(".withdraw");
+  all.classList.remove("button-filter");
+  withdraw.classList.remove("button-filter");
+  const filter = filterByCategory(insertedValues, 1);
+  addFinancialRecord(filter);
+  addValueSum(filter);
+  noRecordedValues(filter);
+});
+
+const withdraw = document.querySelector(".withdraw");
+withdraw.addEventListener("click", () => {
+  withdraw.classList.toggle("button-filter");
+  const all = document.querySelector(".all");
+  const deposit = document.querySelector(".deposit");
+  all.classList.remove("button-filter");
+  deposit.classList.remove("button-filter");
+  const filter = filterByCategory(insertedValues, 2);
+  addFinancialRecord(filter);
+  addValueSum(filter);
+  noRecordedValues(filter);
+});
+
+noRecordedValues(insertedValues);
+addFinancialRecord(insertedValues);
+addValue();
+addValueSum(insertedValues);
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,141 @@
-/* Desenvolva sua lógica aqui */
+let clickedBtn = null;
+
+function openModal() {
+  const main = document.getElementById("app");
+
+  const divScreen = document.createElement("div");
+  const divModal = document.createElement("div");
+  const sectionHeader = document.createElement("section");
+  const h3 = document.createElement("h3");
+  const buttonClose = document.createElement("button");
+  const sectionBody = document.createElement("body");
+  const pText = document.createElement("p");
+  const pValue = document.createElement("p");
+  const input = document.createElement("input");
+  const pType = document.createElement("p");
+  const buttonDeposit = document.createElement("button");
+  const buttonWithdraw = document.createElement("button");
+  const divButtonsModal = document.createElement("div");
+  const buttonCancel = document.createElement("button");
+  const buttonInsert = document.createElement("button");
+
+  divScreen.classList.add(
+    "div-screen",
+    "full",
+    "bg-grey-opacity",
+    "position-fix",
+    "top-left",
+    "flex",
+    "align-center",
+    "justify-center"
+  );
+  divModal.classList.add("div-modal", "bg-white", "padding-2");
+  sectionHeader.classList.add(
+    "section-header-modal",
+    "flex",
+    "justify-between",
+    "align-center",
+    "margin-3"
+  );
+  h3.classList.add("title-modal", "title-2-bold", "color-grey-1");
+  buttonClose.classList.add(
+    "button-close-modal",
+    "padding-3",
+    "bg-grey-5",
+    "color-grey-2",
+    "title-2-bold"
+  );
+  sectionBody.classList.add("section-body-modal", "flex", "flex-col");
+  pText.classList.add("color-grey-2", "text-1-bold", "margin-3");
+  pValue.classList.add("color-grey-1", "text-2-medium", "margin-4");
+  input.classList.add("input-modal", "padding-4", "margin-3");
+  pType.classList.add("color-grey-1", "text-2-medium", "margin-4");
+  buttonDeposit.classList.add(
+    "button-deposit-modal",
+    "margin-4",
+    "text-3-medium"
+  );
+  buttonWithdraw.classList.add(
+    "button-withdraw-modal",
+    "margin-3",
+    "text-3-medium"
+  );
+  divButtonsModal.classList.add("flex", "justify-between");
+  buttonCancel.classList.add("button-cancel-modal", "text-3-medium");
+  buttonInsert.classList.add("button-insert-modal", "text-3-medium");
+
+  buttonDeposit.addEventListener("click", () => {
+    clickedBtn = 1;
+  });
+  buttonWithdraw.addEventListener("click", () => {
+    clickedBtn = 2;
+  });
+
+  h3.innerText = "Registro de valor";
+  buttonClose.innerText = "X";
+  buttonClose.addEventListener("click", () => {
+    const divScn = document.querySelector(".div-screen");
+    divScn.remove();
+  });
+  pText.innerText =
+    "Digite o valor e em seguida aperte no botão referente ao tipo de valor";
+  pValue.innerText = "Valor";
+  input.type = "number";
+  input.placeholder = "R$ 00,00";
+  pType.innerText = "Tipo de valor";
+  buttonDeposit.innerText = "Entrada";
+  buttonWithdraw.innerText = "Saída";
+  buttonDeposit.addEventListener("click", () => {
+    buttonDeposit.classList.toggle("button-clicked");
+    buttonWithdraw.classList.remove("button-clicked");
+  });
+
+  buttonWithdraw.addEventListener("click", () => {
+    buttonWithdraw.classList.toggle("button-clicked");
+    buttonDeposit.classList.remove("button-clicked");
+  });
+
+  buttonCancel.innerText = "Cancelar";
+  buttonCancel.addEventListener("click", () => {
+    const divScn = document.querySelector(".div-screen");
+    divScn.remove();
+  });
+
+  buttonInsert.innerText = "Inserir valor";
+  buttonInsert.addEventListener("click", () => {
+    if (clickedBtn && input.value > 0) {
+      const item = {
+        id: insertedValues.length + 1,
+        value: Number(input.value),
+        categoryID: clickedBtn,
+      };
+      insertedValues.push(item);
+      addFinancialRecord(insertedValues);
+      clickedBtn = null;
+      const divScn = document.querySelector(".div-screen");
+      divScn.remove();
+    } else if (!clickedBtn) {
+      alert("Selecione o tipo de valor!");
+    } else if (input.value < 0) {
+      input.value = 0;
+      alert("Digite um valor válido!");
+    }
+  });
+
+  main.appendChild(divScreen);
+  divScreen.appendChild(divModal);
+  divModal.append(sectionHeader, sectionBody);
+  sectionHeader.append(h3, buttonClose);
+  sectionBody.append(
+    pText,
+    pValue,
+    input,
+    pType,
+    buttonDeposit,
+    buttonWithdraw,
+    divButtonsModal
+  );
+  divButtonsModal.append(buttonCancel, buttonInsert);
+
+  return divScreen;
+}
